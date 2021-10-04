8d3a1c0435aeb7f41d5bb909b82df92051b07032
Alex <alejandrocarreirar@gmail.com>
2021-10-04 12:08:23 +0200

    2nd version

--- e1/src/e1/StringCount.java
+++ e1/src/e1/StringCount.java
@@ -1,70 +1,105 @@
 package e1;
 
+import java.util.Locale;
+
 public class StringCount {
 
-    public static int countWords(String text){
-        char ch;
-        int cnt = 0;
-
+    public static int countWords(String text) {
         if(text == null){
             return 0;
-        }else{
-            for (int n = 0;n<=text.length();n++){
-                ch = text.charAt(n);
-                if(ch == ' '){
-                    cnt++;
-                }
-            }
-            return cnt;
         }
+        text.trim();
+        String Datos[] = text.split(" ");
+        int cnt = 0;
+        for(int i = 0; i<Datos.length; i++){
+            Datos[i].trim();
+            if(Datos[i].length()==0){
+                continue;
+            }
+            cnt++;
+        }
+        return cnt;
 
     }
 
     public static int countChar(String text,char c){
-        char ch;
-        int cnt = 0;
-
+        int cnt = 0, pos;
         if(text == null){
             return 0;
-        }else{
-            for (int n = 0;n<=text.length();n++){
-                ch = text.charAt(n);
-                if(ch == c){
-                    cnt++;
-                }
-            }
-            return cnt;
         }
-
+        pos = text.indexOf(c);
+        while(pos!=-1){
+            cnt++;
+            pos = text.indexOf(c, pos + 1);
+        }
+        return cnt;
     }
 
     public static int countCharIgnoringCase(String text,char c){
-        char ch;
-        int cnt = 0;
-
-        char d = Character.toLowerCase(c);
-        char C = Character.toUpperCase(c);
-        if (text == null){
+        int cnt = 0, pos,n=0,i=0;
+        if(text == null){
             return 0;
-        }else {
-            for (int n = 0; n <= text.length(); n++) {
-                ch = text.charAt(n);
-                if (ch == d || ch == C) {
-                    cnt++;
-                }
-            }
-            return cnt;
         }
+        String textominusculas[] = text.split("");
+        while (n< textominusculas.length){
+            textominusculas[n] = textominusculas[n].toLowerCase();
+            n++;
+        }
+        while (i< textominusculas.length){
+            if(textominusculas[i] == c){
+
+            }
+            i++;
+        }
+        while(pos!=-1){
+            cnt++;
+            pos = textominusculas.indexOf(c, pos + 1);
+        }
+        return cnt;
+
+
     }
 
     public static boolean isPasswordSafe(String password){
         char ch;
         int cntlength;
-        int cntuc;
-        int cntlc;
-        int cntdig;
-        int cntchar;
+        int cntuc = 0, cntlc = 0, cntdig = 0, cntchar = 0;
 
+
+        if(password.length()<8){
+            return false;
+        }else{
+            for(int n = 0; n<=password.length(); n++){
+                if(Character.isUpperCase(password.charAt(n))){
+                    cntuc++;
+                }
+            }
+            if(cntuc<1){
+                return false;
+            } else{
+                for(int n = 0; n<=password.length(); n++) {
+                    if (Character.isLowerCase(password.charAt(n))) {
+                     cntlc++;
+                    }
+                }
+                if(cntlc<1){
+                    return false;
+                } else{
+                    for(int n = 0; n<=password.length(); n++) {
+                        if (Character.isDigit(password.charAt(n))) {
+                            cntdig++;
+                        }
+                    }
+                    if(cntdig<1){
+                        return false;
+                    }else{
+
+                    }
+                }
+            }
+
+
+        }
 
         return true;
     }

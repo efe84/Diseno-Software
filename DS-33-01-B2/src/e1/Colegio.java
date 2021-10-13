e3459e436bd1fca41d30f0a11dc93c50731e8a4d
David Fernandez <d.fernandez4@udc.es>
2021-10-13 09:41:17 +0200

    First upload Boletin2

---
+++ DS-33-01-B2/src/e1/Colegio.java
@@ -0,0 +1,12 @@
+package e1;
+
+public class Colegio {
+
+    public String imprimirRecompensas(){
+        return "0";
+    }
+
+    public String imprimirSalarios(){
+        return "0";
+    }
+}

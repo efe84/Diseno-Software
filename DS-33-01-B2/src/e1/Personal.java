42e3a69c9eed3239494d4a90ce0ad69168e6e578
David Fernandez <d.fernandez4@udc.es>
2021-11-06 00:44:06 +0100

    Exercise 1 done,Not tested yet

--- DS-33-01-B2/src/e1/Personal.java
+++ DS-33-01-B2/src/e1/Personal.java
@@ -2,9 +2,7 @@ package e1;
 
 public class Personal extends Integrantes{
     public int salario;
-
-    public Personal(){
-    }
+    
     public Personal(String nombre, String apellido, int edad, float horrocruxes){
         super(nombre,apellido,edad,horrocruxes);
     }
--- DS-33-01-B2/src/e3/Network.java
+++ DS-33-01-B2/src/e3/Network.java
@@ -1,4 +1,14 @@
 package e3;
 
+import java.util.List;
+
+interface intNetwork{
+    void addUser(String user, List<TopicOfInterest> topicOfInterests);
+    void removeUser(String user);
+    void addInterest(String user, TopicOfInterest topicOfInterest);
+    List<String> getUsers();
+    List<TopicOfInterest> getIterests();
+    List<TopicOfInterest> getInterstsUser(String user);
+}
 public class Network {
 }

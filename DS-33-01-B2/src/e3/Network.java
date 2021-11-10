dcff33f8eef223ece2ad99cf5e75c8eb45f72db3
David Fernandez <d.fernandez4@udc.es>
2021-11-10 08:47:29 +0100

    Exercise 1 done,Not tested yet

--- DS-33-01-B2/src/e3/Network.java
+++ DS-33-01-B2/src/e3/Network.java
@@ -2,13 +2,8 @@ package e3;
 
 import java.util.List;
 
-interface intNetwork{
-    void addUser(String user, List<TopicOfInterest> topicOfInterests);
-    void removeUser(String user);
-    void addInterest(String user, TopicOfInterest topicOfInterest);
-    List<String> getUsers();
-    List<TopicOfInterest> getIterests();
-    List<TopicOfInterest> getInterstsUser(String user);
+interface TopicOfInterest{
+    
 }
 public class Network {
 }
